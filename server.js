06ab36932d4530048ac5b9024935bf1b7183d274
Patrick <patrickawong@gmail.com>
2017-12-05 19:21:08 -0800

    ashboh working

--- server.js
+++ server.js
@@ -1,72 +1,39 @@
-var express = require('express');
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
 var app = express();
-var passport = require('passport');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-var env = require('dotenv').load();
-var exphbs = require('express-handlebars') // set up view
-var path = require('path');
-var publicPath =  path.join(__dirname, './app/public');
-//For BodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
+var PORT = process.env.PORT || 8080;
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use('/', express.static(publicPath));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// For Passport
+// Static directory
+app.use(express.static("./public"));
 
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
-
-app.use(passport.initialize());
-
-app.use(passport.session()); // persistent login sessions
-
-//For Handlebars
-app.set('views', './app/views')
-app.engine("hbs", exphbs({ defaultLayout: "main" }));
-// app.engine('hbs', exphbs({
-//     extname: '.hbs'
-// }));
-
-app.set('view engine', '.hbs');
-
-app.get('/', function(req, res) {
-
-    // res.send('Welcome to Passport with Sequelize');
-res.render('signin.hbs');
-});
-//test
-app.get('/home', function(req, res) {
-  res.render('main.hbs');
-})
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 
-//Models
-var models = require("./app/models");
-
-// import this route in our server.js and pass app as an argument.
-//Routes
-var authRoute = require('./app/routes/auth.js')(app,passport);
-
-//load passport strategies : below route import
-require('./app/config/passport/passport.js')(passport, models.user);
-
-//Sync Database
-models.sequelize.sync().then(function() {
-
-    console.log('Nice! Database looks fine')
-
-}).catch(function(err) {
-
-    console.log(err, "Something went wrong with the Database Update!")
-
-});
-
-
-
-app.listen(5000, function(err) {
-
-    if (!err)
-        console.log("Site is live");
-    else console.log(err)
-
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
